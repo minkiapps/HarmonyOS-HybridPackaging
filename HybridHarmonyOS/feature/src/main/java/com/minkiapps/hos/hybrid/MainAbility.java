51db91d21f8ab1e34600468d5bdfef1bfbe306f3
minkiapps <minkiapps@gmail.com>
2021-07-20 16:19:01 +0200

    Adapt form card, add button to directly open activity

--- HybridHarmonyOS/feature/src/main/java/com/minkiapps/hos/hybrid/MainAbility.java
+++ HybridHarmonyOS/feature/src/main/java/com/minkiapps/hos/hybrid/MainAbility.java
@@ -1,108 +1,75 @@
 package com.minkiapps.hos.hybrid;
 
 import com.minkiapps.hos.hybrid.slice.MainAbilitySlice;
-import com.minkiapps.hos.hybrid.widget.controller.FormController;
-import com.minkiapps.hos.hybrid.widget.controller.FormControllerManager;
 import ohos.aafwk.ability.Ability;
 import ohos.aafwk.content.Intent;
-import ohos.aafwk.ability.AbilitySlice;
 import ohos.aafwk.ability.ProviderFormInfo;
+import ohos.aafwk.content.Operation;
+import ohos.agp.components.ComponentProvider;
+import ohos.event.intentagent.IntentAgent;
+import ohos.event.intentagent.IntentAgentConstant;
+import ohos.event.intentagent.IntentAgentHelper;
+import ohos.event.intentagent.IntentAgentInfo;
 import ohos.hiviewdfx.HiLog;
 import ohos.hiviewdfx.HiLogLabel;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainAbility extends Ability {
-    public static final int DEFAULT_DIMENSION_2X2 = 2;
-    private static final int INVALID_FORM_ID = -1;
+
     private static final HiLogLabel TAG = new HiLogLabel(HiLog.DEBUG, 0x0, MainAbility.class.getName());
-    private String topWidgetSlice;
 
     @Override
     public void onStart(Intent intent) {
         super.onStart(intent);
         super.setMainRoute(MainAbilitySlice.class.getName());
-        if (intentFromWidget(intent)) {
-            topWidgetSlice = getRoutePageSlice(intent);
-            if (topWidgetSlice != null) {
-                setMainRoute(topWidgetSlice);
-            }
-        }
         stopAbility(intent);
     }
 
     @Override
     protected ProviderFormInfo onCreateForm(Intent intent) {
         HiLog.info(TAG, "onCreateForm");
-        long formId = intent.getLongParam(AbilitySlice.PARAM_FORM_IDENTITY_KEY, INVALID_FORM_ID);
-        String formName = intent.getStringParam(AbilitySlice.PARAM_FORM_NAME_KEY);
-        int dimension = intent.getIntParam(AbilitySlice.PARAM_FORM_DIMENSION_KEY, DEFAULT_DIMENSION_2X2);
-        HiLog.info(TAG, "onCreateForm: formId=" + formId + ",formName=" + formName);
-        FormControllerManager formControllerManager = FormControllerManager.getInstance(this);
-        FormController formController = formControllerManager.getController(formId);
-        formController = (formController == null) ? formControllerManager.createFormController(formId,
-                formName, dimension) : formController;
-        if (formController == null) {
-            HiLog.error(TAG, "Get null controller. formId: " + formId + ", formName: " + formName);
-            return null;
-        }
-        return formController.bindFormData();
+        final ProviderFormInfo formInfo = new ProviderFormInfo(ResourceTable.Layout_form_image_with_information_widget_2_2, this);
+        final ComponentProvider componentProvider = new ComponentProvider();
+        componentProvider.setIntentAgent(ResourceTable.Id_btn_form_goto_activity, startAbilityIntentAgent());
+        formInfo.mergeActions(componentProvider);
+        return formInfo;
+    }
+
+    private IntentAgent startAbilityIntentAgent() {
+        final Intent intent = new Intent();
+        final Operation operation = new Intent.OperationBuilder()
+                .withFlags(Intent.FLAG_NOT_OHOS_COMPONENT)
+                .withDeviceId("")
+                .withBundleName(MyApplication.ANDROID_BUNDLE_NAME)
+                .withAbilityName(MyApplication.ANDROID_ANOTHER_ABILITY_NAME)
+                .build();
+        intent.setOperation(operation);
+        final List<Intent> intentList = new ArrayList<>();
+        intentList.add(intent);
+        final List<IntentAgentConstant.Flags> flags = new ArrayList<>();
+        flags.add(IntentAgentConstant.Flags.UPDATE_PRESENT_FLAG);
+        final IntentAgentInfo paramsInfo = new IntentAgentInfo(10001,
+                IntentAgentConstant.OperationType.START_ABILITY, flags, intentList, null);
+        return IntentAgentHelper.getIntentAgent(this, paramsInfo);
     }
 
     @Override
     protected void onUpdateForm(long formId) {
         HiLog.info(TAG, "onUpdateForm");
         super.onUpdateForm(formId);
-        FormControllerManager formControllerManager = FormControllerManager.getInstance(this);
-        FormController formController = formControllerManager.getController(formId);
-        formController.updateFormData(formId);
     }
 
     @Override
     protected void onDeleteForm(long formId) {
         HiLog.info(TAG, "onDeleteForm: formId=" + formId);
         super.onDeleteForm(formId);
-        FormControllerManager formControllerManager = FormControllerManager.getInstance(this);
-        formControllerManager.deleteFormController(formId);
     }
 
     @Override
     protected void onTriggerFormEvent(long formId, String message) {
         HiLog.info(TAG, "onTriggerFormEvent: " + message);
         super.onTriggerFormEvent(formId, message);
-        FormControllerManager formControllerManager = FormControllerManager.getInstance(this);
-        FormController formController = formControllerManager.getController(formId);
-        formController.onTriggerFormEvent(formId, message);
-    }
-
-    @Override
-    public void onNewIntent(Intent intent) {
-        if (intentFromWidget(intent)) { // Only response to it when starting from a service widget.
-            String newWidgetSlice = getRoutePageSlice(intent);
-            if (topWidgetSlice == null || !topWidgetSlice.equals(newWidgetSlice)) {
-                topWidgetSlice = newWidgetSlice;
-                restart();
-            }
-        }
-    }
-
-    private boolean intentFromWidget(Intent intent) {
-        long formId = intent.getLongParam(AbilitySlice.PARAM_FORM_IDENTITY_KEY, INVALID_FORM_ID);
-        return formId != INVALID_FORM_ID;
-    }
-
-    private String getRoutePageSlice(Intent intent) {
-        long formId = intent.getLongParam(AbilitySlice.PARAM_FORM_IDENTITY_KEY, INVALID_FORM_ID);
-        if (formId == INVALID_FORM_ID) {
-            return null;
-        }
-        FormControllerManager formControllerManager = FormControllerManager.getInstance(this);
-        FormController formController = formControllerManager.getController(formId);
-        if (formController == null) {
-            return null;
-        }
-        Class<? extends AbilitySlice> clazz = formController.getRoutePageSlice(intent);
-        if (clazz == null) {
-            return null;
-        }
-        return clazz.getName();
     }
 }
